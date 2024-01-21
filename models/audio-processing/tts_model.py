b88efe924fc04d6efa18bb43e696a6e329b53747
Ak1 <github@maurice-johnson.de>
2024-01-21 22:05:11 +0100

    Restored old tts file

--- models/audio-processing/tts_model.py
+++ models/audio-processing/tts_model.py
@@ -1,33 +1,7 @@
-from flask import Flask, request, jsonify
-from tts_model import use_tts
+from gtts import gTTS
 
-app = Flask(__name__)
+# SEARCH FOR BETTER MODEL, THIS ISN'T GOOD
 
-@app.route("/")
-def hello_world():
-    return "<p>MilkyTeadrop Audio Processing Api</p>"
-
-@app.route('/api/audio/tts', methods=['POST'])
-def api_tts():
-    if request.method == 'POST':
-        json_data = request.json
-        message_to_transcribe = json_data.get('message')
-
-        if not message_to_transcribe:
-            return handle_bad_request()
-
-        print("Message to transcribe is: ", message_to_transcribe)
-        base64 = use_tts(message_to_transcribe=message_to_transcribe)
-
-        return jsonify({"status": "ok", "base64": base64})
-    else:
-        return handle_bad_request()
-
-def handle_bad_request():
-    return jsonify({
-        "status": "error",
-        "message": "'message' is missing in request"
-    }), 400
-
-if __name__ == '__main__':
-    app.run()
+def use_tts(message_to_transcribe: str):
+    tts = gTTS(message_to_transcribe)
+    tts.save('./../data/output.mp3')
